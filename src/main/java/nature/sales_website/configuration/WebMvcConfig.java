bd8f7170a22b5c2fbb50105ea1d043fff3a3e8d4
Quang Van Tiep <quangvantiepp@gmail.com>
2024-12-06 23:40:29 +0700

    add some change for test deploy on the hosting Render

--- src/main/java/nature/sales_website/configuration/WebMvcConfig.java
+++ src/main/java/nature/sales_website/configuration/WebMvcConfig.java
@@ -9,7 +9,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://bd36-171-224-180-164.ngrok-free.app/")
+        // quangvantiepp.kesug.com website deploy on the infinity free
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "quangvantiepp.kesug.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
